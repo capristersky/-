47afabaec2e74bb02cfdf6f35fe68cb38bc015c3
capristersky <ysshin1202@gmail.com>
2025-08-21 15:34:45 +0900

    [Bronze V] Title: A+B - 8, Time: 144 ms, Memory: 16304 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄에서 공백 분리
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int sum = a + b;
+			
+			// 출력
+			bw.write("Case #" + test_case + ": " + a + " + " + b + " = " + (a + b));
+			bw.newLine();
+			
+		}
+		
+		// 마지막에 한 번만 flush
+        bw.flush();
+	}
+}
