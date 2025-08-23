09ed9650819aaa15cb7d72dc985a94b20a9fff11
capristersky <ysshin1202@gmail.com>
2025-08-23 21:02:10 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean[] submitted = new boolean[31]; // 1~30까지 쓰려고
+		
+		for (int i = 1; i <= 28; i++) {
+			int n = sc.nextInt();
+			submitted[n] = true;
+		}
+		
+		for (int i = 1; i <= 30; i++) {
+			if(!submitted[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+}
